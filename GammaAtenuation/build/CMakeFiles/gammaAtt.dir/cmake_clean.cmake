6f4e9f775b1e0e164b7caaa93be6e545e9ab5592
PoPPop21 <sopamalagon@gmail.com>
2025-09-27 14:23:46 -0500

    Creo la funcion make y el cmake

---
+++ GammaAtenuation/build/CMakeFiles/gammaAtt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gammaAtt.dir/src/main.cc.o"
+  "CMakeFiles/gammaAtt.dir/src/main.cc.o.d"
+  "gammaAtt"
+  "gammaAtt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gammaAtt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GammaAtenuation/build/CMakeFiles/gammaAtt.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gammaAtt.
+# This may be replaced when dependencies are built.
